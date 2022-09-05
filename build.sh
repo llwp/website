feb67ca41cc49091fa4ef3b5c5ca9b9d8178e017
Efra Peralta <efra23peralta@gmail.com>
2022-09-05 16:09:34 -0400

    ci: add command to install deps

--- build.sh
+++ build.sh
@@ -4,14 +4,17 @@ set -e
 
 # Build sources for landing application
 cd wwww
+npm install
 npm run build
 npm run export
 
 # Build sources for marketplace application
 cd ../marketplace
+npm install
 npm run build
 npm run export
 
 # Build sources for docs application
 cd ../docs
+npm install
 npm run build
