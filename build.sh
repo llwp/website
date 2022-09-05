841e9f085092663756f1f31e4670529b9e62ccca
Efra Peralta <efra23peralta@gmail.com>
2022-09-05 16:15:43 -0400

    ci: fix deps installation

--- build.sh
+++ build.sh
@@ -4,17 +4,17 @@ set -e
 
 # Build sources for landing application
 cd ./www
-npm install
+npm install --force
 npm run build
 npm run export
 
 # Build sources for marketplace application
 cd ../marketplace
-npm install
+npm install --force
 npm run build
 npm run export
 
 # Build sources for docs application
 cd ../docs
-npm install
+npm install --force
 npm run build
